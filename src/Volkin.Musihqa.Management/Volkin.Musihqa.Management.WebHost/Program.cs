e93a16109edebcaf36b49e0af13255821c76559d
V_O <v9160628646@gmail.com>
2022-08-31 23:44:34 +0300

    add Startup, refactor Program old fashioned way

--- src/Volkin.Musihqa.Management/Volkin.Musihqa.Management.WebHost/Program.cs
+++ src/Volkin.Musihqa.Management/Volkin.Musihqa.Management.WebHost/Program.cs
@@ -1,90 +1,62 @@
-using System.Reflection;
-using Microsoft.EntityFrameworkCore;
 using Serilog;
-using Volkin.Musihqa.Management.Application.Common;
-using Volkin.Musihqa.Management.DataAccess.Common;
 using Volkin.Musihqa.Management.DataAccess.Data;
-using Volkin.Musihqa.Management.DataAccess.Repositories;
-using Volkin.Musihqa.Management.Domain.Abstractions;
 using Volkin.Musihqa.Management.WebHost.Common.Extensions;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Logging.ClearProviders();
-builder.Logging.AddSerilog();
-//builder.Logging.AddConsole();
-
-// Add services to the container
-builder.Services.AddControllers(x => x.SuppressAsyncSuffixInActionNames = true);
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddScoped<IAlbumRepository, AlbumRepository>();
-builder.Services.AddScoped<IArtistRepository, ArtistRepository>();
-
-builder.Services.AddScoped<IManagementUnitOfWork, ManagementUnitOfWork>();
-builder.Services.AddScoped<IDbInitializer, EfDbInitializer>();
-
-builder.Services.AddApplication();
-
-string? connectionString = builder.Configuration.GetConnectionString("ManagementDatabase");
-builder.Services.AddDbContext<DataContext>(x =>
+namespace Volkin.Musihqa.Management.WebHost
 {
-    x.UseNpgsql(connectionString);
-});
-
-builder.Services.AddSwaggerGen(options =>
-{
-    // Set the comments path for the Swagger JSON and UI.
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
-
-string? environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-IConfigurationRoot? configuration = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{environment}.json", optional: true)
-    .Build();
-
-LoggingExtensions.ConfigureLogging(environment, configuration);
-
-try
-{
-    Log.Information("Starting host");
-
-    WebApplication app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment()
-        || app.Environment.EnvironmentName is "Docker")
+    public static class Program
     {
-        app.UseSwagger(options =>
+        public static void Main(string[] args)
         {
-            options.SerializeAsV2 = true;
-        });
+            try
+            {
+                Log.Information("Creating host builder");
+                IHostBuilder builder = CreateWebHostBuilder(args);
 
-        app.UseSwaggerUI();
+                Log.Information("Building host");
+                IHost? app = builder.Build();
+
+                using (IServiceScope scope = app.Services.CreateScope())
+                {
+                    scope.ServiceProvider.GetService<IDbInitializer>()?.InitializeDb();
+                }
+
+                Log.Information("Starting host");
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host failure");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static IHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                    });
+
+            builder.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddSerilog();
+            });
+
+            string? environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            IConfigurationRoot? configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{environment}.json", true)
+                .Build();
+
+            LoggingExtensions.ConfigureLogging(environment, configuration);
+
+            return builder;
+        }
     }
-
-    app.ConfigureCustomExceptionMiddleware();
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    using IServiceScope scope = app.Services.CreateScope();
-    scope.ServiceProvider.GetService<IDbInitializer>()?.InitializeDb();
-
-    app.Run();
 }
-catch (Exception ex)
-{
-    Log.Fatal(ex, "Host failure.");
-}
-finally
-{
-    Log.CloseAndFlush();
-}
-
-
---
+++ src/Volkin.Musihqa.Management/Volkin.Musihqa.Management.WebHost/Startup.cs
@@ -0,0 +1,74 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Volkin.Musihqa.Management.Application.Common;
+using Volkin.Musihqa.Management.DataAccess.Common;
+using Volkin.Musihqa.Management.DataAccess.Data;
+using Volkin.Musihqa.Management.DataAccess.Repositories;
+using Volkin.Musihqa.Management.Domain.Abstractions;
+using Volkin.Musihqa.Management.WebHost.Common.Extensions;
+
+namespace Volkin.Musihqa.Management.WebHost
+{
+    public class Startup
+    {
+        private readonly IConfiguration _сonfiguration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _сonfiguration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IAlbumRepository, AlbumRepository>();
+            services.AddScoped<IArtistRepository, ArtistRepository>();
+            
+            services.AddScoped<IManagementUnitOfWork, ManagementUnitOfWork>();
+            services.AddScoped<IDbInitializer, EfDbInitializer>();
+            
+            services.AddApplication();
+
+            string? connectionString = _сonfiguration.GetConnectionString("ManagementDatabase");
+            services.AddDbContext<DataContext>(x =>
+            {
+                x.UseNpgsql(connectionString);
+            });
+
+            services.AddControllers(x => x.SuppressAsyncSuffixInActionNames = true);
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen(options =>
+            {
+                // Set the comments path for the Swagger JSON and UI
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment() || env.EnvironmentName is "Docker")
+            {
+                app.UseSwagger(options =>
+                {
+                    options.SerializeAsV2 = true;
+                });
+
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Musihqa v1"));
+            }
+
+            app.ConfigureCustomExceptionMiddleware();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
